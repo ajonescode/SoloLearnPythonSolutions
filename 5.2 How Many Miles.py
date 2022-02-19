8aec7cbc72c5f06d24d9c9ba7eb77584f7134a51
Tony Jones Jr <54915239+ajonescode@users.noreply.github.com>
2022-02-18 19:26:48 -0600

    Add files via upload

---
+++ 5.2 How Many Miles.py
@@ -0,0 +1,13 @@
+"""""Quotient
+
+
+Calculate and output the number of miles in 1000 kilometers.
+
+Hint
+One mile is 1.6 kilometers, so find the quotient of 1000 and 1.6. """
+
+#your code goes here
+
+quotient = 1000//1.6
+
+print(float(quotient))
