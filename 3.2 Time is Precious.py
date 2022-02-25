8eebf8404c105d6ecc68fb259c40e30d5050ca99
Tony Jones Jr <54915239+ajonescode@users.noreply.github.com>
2022-02-25 13:34:39 -0600

    Add files via upload

---
+++ 3.2 Time is Precious.py
@@ -0,0 +1,9 @@
+"""Simple Operations
+
+
+Ever wondered how many seconds are there in a month (30 days)?
+
+Write a program to calculate and output the answer. """
+
+#your code goes here
+print (30*24*60*60)
